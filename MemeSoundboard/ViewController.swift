23e8ba87c5d0d3a55666681ce30c2ca3b8804d0c
jgarr7 <33227235+jgarr7@users.noreply.github.com>
2018-07-08 15:09:32 -0500

    Initial Commit

---
+++ MemeSoundboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeSoundboard
+//
+//  Created by Jaden Garrett on 7/8/18.
+//  Copyright © 2018 Jaden Garrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
