396b61a28927c8aa076d5fae404bd6afc5bf4376
jgarr7 <33227235+jgarr7@users.noreply.github.com>
2018-07-10 00:08:33 -0500

    Ui finished (hopefully)

--- MemeSoundboard/ViewController.swift
+++ MemeSoundboard/ViewController.swift
@@ -7,9 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
+
+
 
 class ViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer?
+    
+    func playSoundWithFileName(file: String, fileExt: String)-> Void {
+        let audioSourceURL: URL!
+        
+        audioSourceURL = Bundle.main.url(forResource: file, withExtension: fileExt)
+        
+        if audioSourceURL == nil{
+            print("No Audio")
+        }
+        else{
+            do {
+                audioPlayer = try AVAudioPlayer.init(contentsOf: audioSourceURL!)
+                audioPlayer?.prepareToPlay()
+                audioPlayer?.play()
+            } catch{
+                print(error)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +43,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func naniTouched(_ sender: Any) {
+        playSoundWithFileName(file: "Nani!", fileExt: "mp3")
+    }
+    
 
 
 }
